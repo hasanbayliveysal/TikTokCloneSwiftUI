d5791eb714a2a514b5cd73f9a1ae7edf5f2664d6
Veysal Hasanbayov <hasanbayliveysaldev@gmail.com>
2024-12-12 11:03:18 +0400

    Initial Commit

---
+++ TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Veysal Hasanbayov on 12.12.24.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
